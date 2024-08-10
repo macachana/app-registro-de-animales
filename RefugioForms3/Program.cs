75ed522216b95005d27e1a0fd0b2308c861de063
Macarena Chanampa <123892029+macachana@users.noreply.github.com>
2024-08-10 14:18:00 -0300

    Add files via upload

---
+++ RefugioForms3/Program.cs
@@ -0,0 +1,22 @@
+using RefugioClases;
+
+namespace RefugioForms
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            FormLogin main = new FormLogin();
+            main.Show();
+            Application.Run();
+        }
+    }
+}
